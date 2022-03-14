5600dfa5ae8af6f7b3302a166b9ad938d26fa08e
Erez Calderon <erezcal1@gmail.com>
2022-03-14 15:48:23 +0200

    rick and morty  axios

--- 14.3/rickAndMorty/js/index.js
+++ 14.3/rickAndMorty/js/index.js
@@ -1,22 +1,35 @@
+let charsArr = [];
+
 window.addEventListener("load", async () => {
   try {
     let data = await axios.get("https://rickandmortyapi.com/api/character");
-    let containerStr = "";
-    for (let item of data.data.results) {
-      containerStr += `
+    charsArr = data.data.results;
+    displayCards();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const displayCards = () => {
+  let containerStr = "";
+  for (let item of charsArr) {
+    containerStr += `
       <div class="col">
         <div class="card">
           <img src="${item.image}" class="card-img-top" alt="..." style="width: 200px">
           <div class="card-body">
             <h5 class="card-title">${item.name}</h5>
             <p class="card-text">${item.location.name}</p>
+            <button type="button" class="btn btn-danger" onclick="handleDelete(${item.id})" >delete Me</button>
           </div>
         </div>
       </div>
       `;
-    }
-    document.getElementById("card_Container").innerHTML = containerStr;
-  } catch (error) {
-    console.log(error);
   }
-});
+  document.getElementById("card_Container").innerHTML = containerStr;
+};
+
+const handleDelete = (id) => {
+  charsArr = charsArr.filter((item) => item.id !== id);
+  displayCards();
+};
